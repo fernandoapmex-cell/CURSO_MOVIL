db8a3fc0c8533aeaaca128fffaf97b48760a7481
fernandoapmex-cell <fernandoapmex@gmail.com>
2026-01-21 23:40:34 -0600

    Actualizacion Curso

--- funciones.dart
+++ funciones.dart
@@ -1,8 +1,9 @@
 void main() {
   print(greetEveryone());
   print(funcionDeFlechaOLambda());
-  print('suma de 2 valores :${addTwoNumbers(10,20)}');
-    print('suma de 2 valores :${addTwoNumbersLambda(10,20)}');
+  print('suma de 2 valores :${addTwoNumbers(10, 20)}');
+  print('suma de 2 valores :${addTwoNumbersLambda(10, 20)}');
+  print(greetPerson(name:'Fernando'));
 }
 
 String greetEveryone() {
@@ -11,13 +12,16 @@ String greetEveryone() {
 
 String funcionDeFlechaOLambda() => 'Adios a todos';
 
-int addTwoNumbers(int a,int b){
-  return a+b;
+int addTwoNumbers(int a, int b) {
+  return a + b;
 }
 
-int addTwoNumbersLambda(int a,int b)=> a+b;
+int addTwoNumbersLambda(int a, int b) => a + b;
 
-int addTwoNumbersOptional(int a,[int b=0]){
-  return a+b;
+int addTwoNumbersOptional(int a, [int b = 0]) {
+  return a + b;
 }
 
+String greetPerson({ required String name, String message='Hola'}) {
+  return '$message,$name';
+}
