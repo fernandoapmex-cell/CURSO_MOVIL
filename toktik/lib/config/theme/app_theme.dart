7f924beb618f23f2a7f251a248eb85b852f15516
fernandoapmex-cell <fernandoapmex@gmail.com>
2026-02-06 23:08:32 -0600

    Actualizacion Curso

---
+++ toktik/lib/config/theme/app_theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  ThemeData get theme => ThemeData(
+        useMaterial3: true,
+        brightness: Brightness.dark,  
+        
+      );
+}
