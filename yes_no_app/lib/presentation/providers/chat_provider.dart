989932a05e7201218a44ebc8bc48205dac2ccb1a
fernandoapmex-cell <fernandoapmex@gmail.com>
2026-02-10 14:13:33 -0600

    Actualizacion Curso

--- yes_no_app/lib/presentation/providers/chat_provider.dart
+++ yes_no_app/lib/presentation/providers/chat_provider.dart
@@ -3,7 +3,7 @@ import 'package:yes_no_app/domain/entities/message.dart';
 
 class  ChatProvider extends ChangeNotifier {
   // Chat provider implementation
-  List<Message>message = [
+  List<Message>messages = [
     Message(text: 'Hola, ¿Cómo estás?', fromWho: FromWho.me),
     Message(text: 'Hola, estoy bien, ¿Y tú?', fromWho: FromWho.hers),
     Message(text: 'Muy bien, gracias por preguntar', fromWho: FromWho.me),
--- yes_no_app/lib/presentation/screens/chat/chat_screen.dart
+++ yes_no_app/lib/presentation/screens/chat/chat_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:yes_no_app/domain/entities/message.dart';
+import 'package:yes_no_app/presentation/providers/chat_provider.dart';
 import 'package:yes_no_app/presentation/widgets/chat/file_message_bubble.dart';
 import 'package:yes_no_app/presentation/widgets/chat/her_message_bubble.dart';
 import 'package:yes_no_app/presentation/widgets/shared/message_field_box.dart';
@@ -8,6 +11,7 @@ class ChatScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    
     return Scaffold(
       appBar: AppBar(
         leading: Padding(
@@ -29,6 +33,8 @@ class ChatScreen extends StatelessWidget {
 class _ChatView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final chatProvider =  context.watch<ChatProvider>();
+
     return SafeArea(
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 10.0),
@@ -36,11 +42,12 @@ class _ChatView extends StatelessWidget {
           children: [
             Expanded(
               child: ListView.builder(
-                itemCount: 100,
+                itemCount: chatProvider.messages.length,
                 itemBuilder: (context, index) {
-                  return (index % 2 == 0)
-                      ? const HerMessageBubble()
-                      : const MyMessageBubble();
+                  final message = chatProvider.messages[index];
+                  //return (message.fromWho == FromWho.hers)
+                   // ? HerMessageBubble(message: message)
+                   // :/MyMessageBubble(message: message);
                 },
               ),
             ),
