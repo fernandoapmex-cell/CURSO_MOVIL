9401ea5e2fa42c03b7843aafa6ef5c7d7fd7ded2
fernandoapmex-cell <fernandoapmex@gmail.com>
2026-02-10 01:14:40 -0600

    Actualizacion Curso

---
+++ yes_no_app/lib/presentation/providers/chat_provider.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/domain/entities/message.dart';
+
+class  ChatProvider extends ChangeNotifier {
+  // Chat provider implementation
+  List<Message>message = [
+    Message(text: 'Hola, ¿Cómo estás?', fromWho: FromWho.me),
+    Message(text: 'Hola, estoy bien, ¿Y tú?', fromWho: FromWho.hers),
+    Message(text: 'Muy bien, gracias por preguntar', fromWho: FromWho.me),
+  ];
+  Future <void>sendMessage(String text) async{
+    
+  }
+}
