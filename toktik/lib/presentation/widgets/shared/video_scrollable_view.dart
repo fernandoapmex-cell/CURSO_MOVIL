4f6e62a3999580dc9d07737a8f2e706a09293fd2
fernandoapmex-cell <fernandoapmex@gmail.com>
2026-02-18 14:29:11 -0600

    Actualizacion Curso

--- toktik/lib/presentation/widgets/shared/video_scrollable_view.dart
+++ toktik/lib/presentation/widgets/shared/video_scrollable_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:toktik/domain/entities/video_post.dart';
+import 'package:toktik/presentation/widgets/shared/video_buttons.dart';
 
 class VideoScrollableView extends StatelessWidget {
   final List<VideoPost> videos;
@@ -7,15 +8,26 @@ class VideoScrollableView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return PageView(
-      children: [
-        Container(
-          color: Colors.red,
-        ),
-        Container(
-          color: Colors.green,
-        )
+    return PageView.builder(
+      physics: const BouncingScrollPhysics(),
+      scrollDirection: Axis.vertical,
+      itemCount:videos.length ,
+      itemBuilder: (context, index) {
+        final VideoPost=videos[index];
+        return Stack(
+                children: [
+                  //Video player + gradiente
+
+                  //botones + descripción
+                  Positioned(
+                    bottom: 40,
+                    right: 20,
+                    
+                    child: VideoButtons(video: VideoPost)
+                    )
       ],
+        );
+      },
     );
   }
 }
