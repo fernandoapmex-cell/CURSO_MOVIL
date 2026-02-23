477da5bd16f68239c9724d87b60cb1a849da9ebb
fernandoapmex-cell <fernandoapmex@gmail.com>
2026-02-23 01:57:52 -0600

    Actualizacion Curso

--- toktik/lib/main.dart
+++ toktik/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:toktik/config/theme/app_theme.dart';
+import 'package:toktik/infrastructure/datasources/local_video_datasource_impl.dart';
+import 'package:toktik/infrastructure/repositories/video_posts_repository_impl.dart';
 import 'package:toktik/presentation/providers/discover_provider.dart';
 import 'package:toktik/presentation/screens/discover/discover_screen.dart';
 
@@ -8,14 +10,15 @@ void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
+  
   @override
   Widget build(BuildContext context) {
+    final VideoPostsRepository=VideoPostsRepositoryImpl(videoPostsDatasource: LocalVideoDatasourceImpl());
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(
           lazy: false,
-          create:(_)=>DiscoverProvider()..loadNextPage())
+          create:(_)=>DiscoverProvider(videosRepository: VideoPostsRepository)..loadNextPage())
       ],
       child: MaterialApp(
         title: 'Tok Tik',
