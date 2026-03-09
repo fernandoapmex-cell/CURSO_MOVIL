df3415a4bfacb07f1768ec92c9ab8b199faa179e
fernandoapmex-cell <fernandoapmex@gmail.com>
2026-03-09 14:38:42 -0600

    Actualizacion Curso

--- cinemapedia/README.md
+++ cinemapedia/README.md
@@ -1,3 +1,8 @@
 # cinemapedia
 
 A new Flutter project.
+
+#Dev
+
+copiar el.env.template y nomnbrarlo a .env
+cambiar las variables de entorno
--- cinemapedia/lib/main.dart
+++ cinemapedia/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cinemapedia/config/router/app_router.dart';
 import 'package:cinemapedia/config/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
@@ -10,14 +11,10 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routerConfig: appRouter,
       debugShowCheckedModeBanner: false,
       theme: AppTheme().getTheme(),
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
     );
   }
 }
