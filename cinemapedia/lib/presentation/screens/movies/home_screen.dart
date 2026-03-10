18796a3bd5dfb2e40e847c7d66f64a61bc578a9f
fernandoapmex-cell <fernandoapmex@gmail.com>
2026-03-10 10:58:06 -0600

    Actualizacion Curso

--- cinemapedia/lib/presentation/screens/movies/home_screen.dart
+++ cinemapedia/lib/presentation/screens/movies/home_screen.dart
@@ -1,5 +1,5 @@
+import 'package:cinemapedia/config/constants/enviroment.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 class HomeScreen extends StatelessWidget {
   static const name ='home_screen';
   const HomeScreen({super.key});
@@ -8,7 +8,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text(dotenv.env['THE_MOVIEDB_KEY']??'no hay api key'),
+        child: Text(Enviroment.movieDBKey),
       ),
     );
   }
