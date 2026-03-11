83fc6a2d370a8453c7991ae12409ea79e39e0508
fernandoapmex-cell <fernandoapmex@gmail.com>
2026-03-11 11:41:26 -0600

    Actualizacion Curso

--- cinemapedia/lib/main.dart
+++ cinemapedia/lib/main.dart
@@ -4,10 +4,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-Future <void> main()async{
-  await dotenv.load(fileName: ".env");
+Future<void> main() async{
+
+  await dotenv.load(fileName: '.env');
+
   runApp(
-    const ProviderScope(child: MainApp(),)
+    const ProviderScope(child: MainApp() )
   );
 }
 
--- cinemapedia/lib/presentation/screens/movies/home_screen.dart
+++ cinemapedia/lib/presentation/screens/movies/home_screen.dart
@@ -1,29 +1,49 @@
 import 'package:cinemapedia/presentation/providers/providers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-class HomeScreen extends ConsumerStatefulWidget {
-  static const name ='home_screen';
+class HomeScreen extends StatelessWidget {
+
+  static const name = 'home_screen';
+
   const HomeScreen({super.key});
 
   @override
-  HomeScreenState createState() => HomeScreenState();
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: _HomeView()
+    );
+  }
 }
 
-class HomeScreenState extends ConsumerState<HomeScreen> {
+class _HomeView extends ConsumerStatefulWidget {
+  const _HomeView();
+
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends ConsumerState<_HomeView> {
+
   @override
   void initState() {
     super.initState();
-    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
+    
+    ref.read( nowPlayingMoviesProvider.notifier ).loadNextPage();
   }
+
+
   @override
   Widget build(BuildContext context) {
-    final nowPlayingMovies=ref.watch(nowPlayingMoviesProvider);
+
+    final nowPlayingMovies = ref.watch( nowPlayingMoviesProvider );
+
+
     return ListView.builder(
       itemCount: nowPlayingMovies.length,
-      itemBuilder: (context,index){
-        final movie=nowPlayingMovies[index];
+      itemBuilder: (context, index) {
+        final movie = nowPlayingMovies[index];
         return ListTile(
-          title: Text(movie.title),
+          title: Text( movie.title ),
         );
       },
     );
